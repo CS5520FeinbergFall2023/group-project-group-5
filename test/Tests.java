9ea3e54586ca91f9cd9587b43e8d92e8fcde0ad9
Jiaming Xu <xu.jiami@northeastern.edu>
2023-10-23 14:58:39 -0400

    test case plans

--- test/Tests.java
+++ test/Tests.java
@@ -15,23 +15,76 @@ public class Tests {
 
 //  -------------------Create images that visualize individual R,G,B components of an image.--------
   //create PPM, JPG or PNG given reasonable data
-  //create PPM, JPG or PNG given illegal input ()
+  // png with 8, 24 & 32 bit
+  //create PPM, JPG or PNG given illegal input (negative or too large)
+  //create small img and large img
+  //create monochromatic, dichromatic and trichromatic images
 
 //  -------------Create images that visualize the value, intensity or luma of an image.-------------
-//
+  //different formats
+  // gets the value
+  // intensity
+  // luma
+  // of monochromatic, dichromatic and trichromatic images. Of different values for the pixels or
+  // same value for every pixel.
+
 //  Flip an image horizontally or vertically.
-//
+
+  //different formats
+  //flip horizontally
+  //flip vertically
+  //combination of the above two.
+  //flip for multiple times (more than 2)
+
 //  Brighten or darken an image.
+  //different formats
+  //brighten
+  //darken
+  //pixel that's already the darkest
+  //pixel that's already the lightest
+  //brighten or darken an image for multiple times & in combinations
+
 //
 //  Split a single image into 3 images representing each of the three channels.
-//
+  // different formats
+  // of monochromatic, dichromatic and trichromatic images
+  // repeatedly split
+
 //  Combine three greyscale image into a single color image whose R,G,B values come from the three images.
-//
+  // (at least one of the)input is not legal greyscale image.
+  // different formats
+  // result color is black/white/red/green/blue/mixed color
+
 //  Blur or sharpen an image as defined above.
-//
+  // different formats
+  // of monochromatic, dichromatic and trichromatic images
+  // repeatedly sharpen
+
 //  Convert an image into sepia as defined above.
-//
+  // different formats
+  // of monochromatic, dichromatic and trichromatic images
+  // repeatedly made sepia
+
 //  Save an image to an ASCII PPM, JPG or PNG file (see below).
-//
+  // src image is create directly from this program
+  //src image read from local and directly saved without modification
+  //src image read from local and saved with modification
+  //src format same as saved format
+  //src format different as saved format
+  //different color and size
+
 //  Allow a user to interact with your program to use these operations, using text-based scripting (see below).
+  // problematic input
+  // no comment, single line on command line
+  //with comment, single line
+  //no comment, multiple lines
+  //with comment, multiple lines
+  //perform same tests as above for every function provided here
+  // Load and run the script commands in the specified file.
+        //- ill path
+        //- ill command format
+        //- no comment, single line on command line
+        //- with comment, single line
+        //- no comment, multiple lines
+        //- with comment, multiple lines
 }
