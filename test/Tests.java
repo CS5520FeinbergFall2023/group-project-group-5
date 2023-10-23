3e0a5741f98a046bb329444c36742a90b17c67fd
Jiaming Xu <xu.jiami@northeastern.edu>
2023-10-23 11:41:56 -0400

    initialize

---
+++ test/Tests.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+
+public class Tests {
+
+//  -------------------Load an image from an ASCII PPM, JPG or PNG file.----------------------------
+// test malformed path
+  // well-formed path, but the img does not exist
+  //img exist but not PPM,JPG or PNG extension
+  //is PPM,JPG or PNG extension but actually content not PPM, JPG, PNG
+  //PPM, JPG & PNG, monochromatic(black,white,red,green,blue), dichromatic or trichromatic. With
+  // each value 0,(a number in between 0-255),255
+  //
+  
+//  -------------------Create images that visualize individual R,G,B components of an image.--------
+//
+//  -------------Create images that visualize the value, intensity or luma of an image.-------------
+//
+//  Flip an image horizontally or vertically.
+//
+//  Brighten or darken an image.
+//
+//  Split a single image into 3 images representing each of the three channels.
+//
+//  Combine three greyscale image into a single color image whose R,G,B values come from the three images.
+//
+//  Blur or sharpen an image as defined above.
+//
+//  Convert an image into sepia as defined above.
+//
+//  Save an image to an ASCII PPM, JPG or PNG file (see below).
+//
+//  Allow a user to interact with your program to use these operations, using text-based scripting (see below).
+}
