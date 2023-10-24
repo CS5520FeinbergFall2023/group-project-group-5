12665764a3acdf442e7857511fab82a231086721
spoony912 <jiaoyangdu9@gmail.com>
2023-10-24 16:18:53 -0400

    image class and pixel class

--- src/Image.java
+++ src/Image.java
@@ -1,4 +1,65 @@
+package image;
+
+import java.io.IOException;
+
 public interface Image {
+  /**
+   *
+   * @param operation
+   */
   void takeOperation(Operation operation);
+
+  /**
+   *
+   */
   void performOperations();
+
+  /**
+   * Get the image's name.
+   * @return
+   */
+  String getName();
+
+  /**
+   *
+   * @param fileName
+   * @throws IOException
+   */
+  void load(String fileName) throws IOException;
+
+  /**
+   *
+   * @param fileName
+   * @throws IOException
+   */
+  void save(String fileName) throws IOException;
+
+  /**
+   *
+   * @return
+   */
+  int getHeight();
+
+  /**
+   *
+   * @return
+   */
+  int getWidth();
+
+  /**
+   *
+   * @param x
+   * @param y
+   * @return
+   */
+  Pixel getPixel(int x, int y);
+
+  /**
+   *
+   * @param x
+   * @param y
+   * @param pixel
+   */
+  void setPixel(int x, int y, Pixel pixel);
+
 }
--- src/MyImage.java
+++ src/MyImage.java
@@ -1,20 +1,124 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MyImage implements Image{
+import image.Image;
+
+public class MyImage implements Image {
   private List<Operation> operationList;
   private Pixel[][] pixels;
+  private int height;
+  private int width;
+  private String name;
 
-  public void takeOperation(Operation operation){
+  public MyImage(int height, int width, String name) {
+    if (width <= 0 || height <= 0) {
+      throw new IllegalArgumentException("Invalid width and height.");
+    }
+    if (name == null) {
+      throw new IllegalArgumentException("The name cannot be null");
+    }
+    this.height = height;
+    this.width = width;
+    this.name = name;
+    operationList = new ArrayList<>();
+    pixels = new RGBPixel[height][width];
+    for (int row = 0; row < this.height; row++) {
+      for (int col = 0; col < this.width; col++) {
+        pixels[row][col] = new RGBPixel(0, 0, 0);
+      }
+    }
+  }
+
+  public MyImage(int height, int width, String name, Pixel[][] pixels) {
+    if (width <= 0 || height <= 0) {
+      throw new IllegalArgumentException("Invalid width and height.");
+    }
+    if (name == null) {
+      throw new IllegalArgumentException("The name cannot be null");
+    }
+    if (pixels == null) {
+      throw new IllegalArgumentException("Pixels cannot be null");
+    }
+    this.height = height;
+    this.width = width;
+    this.name = name;
+    this.pixels = pixels;
+    operationList = new ArrayList<>();
+  }
+
+  @Override
+  // 实现加载图像的代码
+  public void load(String fileName) throws IOException {
+
+  }
+
+  @Override
+  // 实现保存图像的代码
+  public void save(String fileName) throws IOException {
+
+  }
+
+  /**
+   * @return
+   */
+  @Override
+  public int getHeight() {
+    return this.height;
+  }
+
+  /**
+   * @return
+   */
+  @Override
+  public int getWidth() {
+    return this.width;
+  }
+
+  public void takeOperation(Operation operation) {
     operationList.add(operation);
   }
 
-  public void performOperations(){
+  public void performOperations() {
     for (Operation operation : operationList) {
       operation.perform();
     }
     operationList.clear();
   }
+
+  /**
+   * Get the image's name.
+   *
+   * @return
+   */
+  @Override
+  public String getName() {
+    return this.name;
+  }
+
+  @Override
+  public Pixel getPixel(int x, int y) {
+    if (x < 0 || x > this.width || y < 0 || y > this.height) {
+      throw new IllegalArgumentException("The x or y is out of bound.");
+    }
+    Pixel pixel = pixels[x][y];
+    return new RGBPixel(pixel.getColorChannel(Channel.Red), pixel.getColorChannel(Channel.Green),
+          pixel.getColorChannel(Channel.Blue));
+
+  }
+
+  @Override
+  public void setPixel(int x, int y, Pixel pixel) {
+    if (pixel == null) {
+      throw new IllegalArgumentException("The pixel cannot be null.");
+    }
+    if (x < 0 || x > this.width || y < 0 || y > this.height) {
+      throw new IllegalArgumentException("The x or y is out of bound.");
+    }
+    pixels[x][y] = pixel;
+  }
+
+
 
 
 }
