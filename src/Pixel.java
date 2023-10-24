f3cf17de3a489224efce824ac7c5bd45f368b618
spoony912 <jiaoyangdu9@gmail.com>
2023-10-24 16:26:39 -0400

    Pixel class and implements

--- src/Pixel.java
+++ src/Pixel.java
@@ -1,2 +1,18 @@
 public interface Pixel {
+
+  /**
+   *
+   * @param channel
+   * @return
+   */
+  int getColorChannel(Channel channel);
+
+  /**
+   *
+   * @param channel
+   * @param color
+   */
+  void setColorChannel(Channel channel, int color);
+
+
 }
