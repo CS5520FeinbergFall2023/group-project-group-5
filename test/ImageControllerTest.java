c8c34af41d9cc4726e2187199fba28ae2b38cd61
spoony912 <jiaoyangdu9@gmail.com>
2023-10-29 17:32:39 -0400

    Update of ImageView and ImageUtil

---
+++ test/ImageControllerTest.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+import java.io.IOException;
+
+
+import controller.ImageController;
+import service.ImageService;
+import view.ImageView;
+
+
+public class ImageControllerTest {
+  private ImageController controller;
+  private ImageService imageServiceMock;
+  private ImageView imageViewMock;
+
+  @Before
+  public void setUp() {
+
+  }
+}
