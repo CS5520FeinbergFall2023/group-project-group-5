126f02e2153c608198432a1838be79a351836d18
spoony912 <jiaoyangdu9@gmail.com>
2023-10-31 22:40:08 -0400

    update controller, controller test

--- src/controller/ImageController.java
+++ src/controller/ImageController.java
@@ -3,9 +3,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
 
@@ -383,5 +381,4 @@ public class ImageController {
     }
     imageView.displayMessage("Please enter command:");
   }
-
 }
