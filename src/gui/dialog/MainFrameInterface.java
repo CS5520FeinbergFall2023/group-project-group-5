8db37dc60d96d58eb22f604c60b68f3b9a37e1f2
Jiaming Xu <xu.jiami@northeastern.edu>
2023-11-21 10:23:44 -0500

    add interface for img

---
+++ src/gui/dialog/MainFrameInterface.java
@@ -0,0 +1,9 @@
+package gui.dialog;
+
+import java.awt.image.BufferedImage;
+
+public interface MainFrameInterface {
+  void updateProcessingImage(BufferedImage image);
+
+  void updateDiagram(BufferedImage diagram);
+}
