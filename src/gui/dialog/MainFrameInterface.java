12bfb159be0fb2509649bf5de6861ae5c5f8b7cf
Jiaming Xu <xu.jiami@northeastern.edu>
2023-11-21 10:36:13 -0500

    change image interface

--- src/gui/dialog/MainFrameInterface.java
+++
@@ -1,9 +0,0 @@
-package gui.dialog;
-
-import java.awt.image.BufferedImage;
-
-public interface MainFrameInterface {
-  void updateProcessingImage(BufferedImage image);
-
-  void updateDiagram(BufferedImage diagram);
-}
